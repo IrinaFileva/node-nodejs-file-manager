457c026789981d4adb6f81a9d94d634374c915c3
Irina Fileva <irinkaskai@gmail.com>
2024-10-14 15:19:21 +0500

    feat: implemented compressFile function

---
+++ src/zip/compress.js
@@ -0,0 +1,46 @@
+import { pipeline } from 'stream/promises';
+import { join, isAbsolute } from 'path';
+import { createBrotliCompress } from 'zlib';
+import { access, constants, stat, rm } from 'fs/promises';
+import { createReadStream, createWriteStream} from 'fs';
+
+export const compressFile = async(workingDir, line) => {
+
+  const args = line.split(' ').filter((item) => item !== '').slice(1);
+
+  if (!args || args.length < 2) {
+    throw new Error('Invalid input');
+  }
+
+  const pathFile = isAbsolute(args[0]) ? args[0] : join(workingDir, args[0]);
+  const pathBrotliFile = isAbsolute(args[1]) ? args[1] : join(workingDir, args[1]);
+
+  try {
+    await access(pathFile, constants.F_OK).catch(() => { throw new Error('Operation failed') });
+
+    const file = await stat(pathFile);
+
+    if (!file.isFile()) throw new Error('Operation failed');
+
+    try {
+      await access(pathBrotliFile);
+      throw new Error('Operation failed\nFile already exists in this folder');
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+
+        const startFile = createReadStream(pathFile);
+        const endFile = createWriteStream(pathBrotliFile);
+        const brotli = createBrotliCompress();
+
+        await pipeline(startFile, brotli, endFile);
+        await rm(pathFile);
+
+        console.log('File compressed');
+      } else {
+        console.error(err.message);
+      }
+    }
+  } catch(err) {
+    console.log(err.message)
+  }
+}
